b398a7f8535cb49289ed6d7db6a329b7e8a5d48c
stepan-bezhuk-axon <sbezhuk@axon.dev>
2025-03-14 23:07:11 +0200

    Initial Commit

---
+++ Shopper-BE/Shopper_BEApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shopper_BEApp.swift
+//  Shopper-BE
+//
+//  Created by Stepan Bezhuk on 14.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct Shopper_BEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
